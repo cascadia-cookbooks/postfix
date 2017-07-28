b3bb3718cf7c10e7e7df078e54de014a0df3cb01
Colby Olson <me@colbyolson.com>
2017-07-28 13:24:11 -0700

    restart the service at the end

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,6 +12,7 @@ template '/etc/postfix/main.cf' do
     source   'default/main.cf.erb'
     action   :create
     notifies :reload, 'service[postfix]', :immediately
+    notifies :restart, 'service[postfix]', :delayed
 end
 
 # template '/etc/postfix/master.cf' do
